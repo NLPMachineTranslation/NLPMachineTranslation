c5c019a7451b67ffdbed912119e5890444d733df
kfpyzi <kfpyzi@gmail.com>
2018-04-29 21:35:17 +0800

    comments for modules

---
+++ fx.py
@@ -0,0 +1,12 @@
+from nltk import * as nlp
+import numpy as np
+import pandas as pd 
+
+#functions 
+#provide sys archi for modules
+
+
+#language model
+#prediction model
+#translation model
+#probability model
